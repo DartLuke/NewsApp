03bc052453520cc7939d57037e29e3a81b98ecd9
dPasser <maddan1024@gmail.com>
2022-02-20 15:59:00 +0200

    -

--- app/src/main/java/com/danielpasser/newsapp/api/Api.kt
+++ app/src/main/java/com/danielpasser/newsapp/api/Api.kt
@@ -5,7 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api {
-
     @GET("top-headlines")
     suspend fun getNews(@Query("apiKey") api_key: String,@Query("country") country:String):NewsResult
 }
