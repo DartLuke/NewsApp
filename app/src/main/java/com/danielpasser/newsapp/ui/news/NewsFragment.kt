982bf459897e0c1f29e740c0c5c27f62357ccd49
dPasser <maddan1024@gmail.com>
2022-02-20 15:59:59 +0200

    -

--- app/src/main/java/com/danielpasser/newsapp/ui/news/NewsFragment.kt
+++ app/src/main/java/com/danielpasser/newsapp/ui/news/NewsFragment.kt
@@ -18,7 +18,7 @@ class NewsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_second, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_news, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
